b34ae7a627375a6195ddd43874787621ac07fca8
chloebertin <35561788+chloebertin@users.noreply.github.com>
2018-01-18 17:31:47 +0100

    organisation des dossier et enrichissement communes adjacentes

---
+++ src/main/R/Enrichissement/2 - Ajout communes adjacentes/Communes adjacentes.R
@@ -0,0 +1,43 @@
+rm(list = ls())
+options(stringsAsFactors = F)
+library(tidyr)
+
+setwd("C:/Users/chloe.bertin/Desktop/Hackathon-2018/données/Enrichissement/Communes adjacentes/")
+
+fichier <- read.csv2("in-data.csv",sep = ";",
+                     colClasses = "character",as.is = T)
+fichier <- fichier[1:100,]
+
+adj <- read.csv("supp-data.csv",sep = ",",
+                     colClasses = "character",as.is = T)
+
+for (i in 1:29) {
+  adj <- cbind(adj,substr(adj$insee_voisins,6*i-5,6*i-1))
+  names(adj)[6+i] <- paste0("com_adj",i)
+  }
+
+adj <- adj %>%
+  select(-nom,-nb_voisins,-insee_voisins,-noms_voisins,-cap_voisins)
+
+
+adj2 <- gather(data=adj,key=COM1,value=COM_ADJ,-insee)
+adj2 <- adj2[,c("insee","COM_ADJ")]
+adj2 <- adj2[adj2$COM_ADJ!="",]
+
+fichier1 <- fichier
+fichier1$INDIC_ORIGINE <- "1"
+
+
+fichier2 <- merge(fichier,adj2,by.x="DEP_COM_CORR",by.y="insee",all.x=T,all.y=F)
+fichier2$INDIC_ORIGINE <- "0"
+fichier2 <- fichier2 %>% 
+  select(-DEP_COM_CORR)
+fichier2$DEP_COM_CORR <- fichier2$COM_ADJ
+fichier2 <- fichier2 %>% 
+  select(-COM_ADJ)
+
+
+fichier_final <- fichier1 %>%
+  bind_rows(fichier2)
+fichier_final <- fichier_final %>%
+  arrange(CABBI)
