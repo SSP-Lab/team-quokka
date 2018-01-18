d53f2c00c48350dd98fd724b93f654166ccaf663
Franck Cotton <franck.cotton@insee.fr>
2018-01-18 20:47:24 +0100

    Travail sur le module CLI

--- src/main/java/fr/insee/sirene/hackathon/CLIModule.java
+++ src/main/java/fr/insee/sirene/hackathon/CLIModule.java
@@ -1,6 +1,11 @@
 package fr.insee.sirene.hackathon;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 public class CLIModule extends ProcessModule {
 
@@ -25,8 +30,11 @@ public class CLIModule extends ProcessModule {
 		    builder.command("sh", commandLine);
 		}
 		builder.directory(new File(SOURCE_ROOT_FOLDER + "/R/" + this.path));
+		builder.redirectErrorStream(true);
 		Process process = builder.start();
-		// Si besoin pour connecter les entrées-sorties : http://www.baeldung.com/run-shell-command-in-java
+		// D'après http://www.baeldung.com/run-shell-command-in-java
+		StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+		Executors.newSingleThreadExecutor().submit(streamGobbler);
 		logger.debug("Lancement de la ligne de commande " + commandLine + " dans le répertoire de travail " + builder.directory().getAbsolutePath());
 		int exitCode = process.waitFor();
 		assert exitCode == 0;
@@ -38,5 +46,20 @@ public class CLIModule extends ProcessModule {
 
 	public void setCommandLine(String commandLine) {
 		this.commandLine = commandLine;
+	}
+
+	private static class StreamGobbler implements Runnable {
+	    private InputStream inputStream;
+	    private Consumer<String> consumer;
+	 
+	    public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+	        this.inputStream = inputStream;
+	        this.consumer = consumer;
+	    }
+	 
+	    @Override
+	    public void run() {
+	        new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(consumer);
+	    }
 	}
 }
