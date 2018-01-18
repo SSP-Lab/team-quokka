5f1ddfd316060497cf4dc8725878bc3b48de81eb
romaintailhurat <romain.tailhurat@gmail.com>
2018-01-18 15:03:56 +0100

    Creation module lecture-rp

---
+++ src/main/R/lecture/lecture-rp/lecture-rp.R
@@ -0,0 +1,22 @@
+rm(list = ls())
+options(stringsAsFactors = F)
+#install.packages("dplyr")
+#install.packages("argparse")
+library(dplyr)
+
+args <- commandArgs(trailingOnly = TRUE)
+cat(args, sep = "\n")
+base_dir <- args[1]
+
+#setwd("C:/Users/chloe.bertin/Desktop/Hackathon-2018/données/Lecture/")
+
+# fichier <- read.csv2("in-data.csv",sep = ";",
+#                      colClasses = "character",as.is = T)
+# param <- read.csv2("in-param.csv",sep = ";",
+#                    colClasses = "character",as.is = T)
+# 
+# fichier <- fichier %>%
+#   select(param$Variables)
+# 
+# write.csv2(fichier,"out-data.csv",row.names = F)
+
