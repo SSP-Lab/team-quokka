1688d89b1bc886924bcebf5722b4543065a85a58
Franck Cotton <franck.cotton@insee.fr>
2018-01-18 16:34:24 +0100

    Travail sur le process

--- src/main/java/fr/insee/sirene/hackathon/ProcessExecution.java
+++ src/main/java/fr/insee/sirene/hackathon/ProcessExecution.java
@@ -5,9 +5,32 @@ public class ProcessExecution {
 	public static void main(String[] args) {
 
 		MainProcess process = new MainProcess();
+		process.setName("Processus principal");
+		process.setPath("process/");
 
 		ProcessStep lecture = new ProcessStep();
+		lecture.setName("Lecture");
+		lecture.setPath("lecture/");
+		ProcessStep enrichissement = new ProcessStep();
+		ProcessStep requetage = new ProcessStep();
+		ProcessStep scoring = new ProcessStep();
+		ProcessStep evaluation = new ProcessStep();
 
+		CLIModule lectureRecensement = new CLIModule();
+		lectureRecensement.setName("Lecture du fichier RP 2017");
+		lectureRecensement.setPath("lecture/lecture-rp/");
+		String path = ProcessComponent.ROOT_FOLDER + "/process/" + lectureRecensement.getPath();
+		lectureRecensement.setInData(ProcessComponent.SOURCE_RP_2017);
+		lectureRecensement.setCommandLine(ProcessComponent.rScript + " lecture-rp.R " + path);
+
+		lecture.addModule(lectureRecensement);
+		process.addStep(lecture);
+
+		try {
+			process.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
