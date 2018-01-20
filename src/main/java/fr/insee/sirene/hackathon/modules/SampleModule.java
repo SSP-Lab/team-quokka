8da079177796526fd2eeb412c558eec1d5025d77
Franck Cotton <franck.cotton@insee.fr>
2018-01-20 17:04:33 +0100

    Fignoleage du module d'échantillonnage

--- src/main/java/fr/insee/sirene/hackathon/modules/SampleModule.java
+++ src/main/java/fr/insee/sirene/hackathon/modules/SampleModule.java
@@ -31,22 +31,25 @@ public class SampleModule extends ProcessModule {
 
 		super.execute();
 		// L'instruction précédente ramène les fichiers d'entrée à leur emplacement par défaut
-		String inputFile = Configuration.PROCESS_ROOT_FOLDER + "/" + this.getPath() + "/in-data.csv";
+		String inputDataFile = Configuration.PROCESS_ROOT_FOLDER + "/" + this.getPath() + "/in-data.csv";
 		String inputParamFile = Configuration.PROCESS_ROOT_FOLDER + "/" + this.getPath() + "/in-param.csv";
 		String outputDataFile = Configuration.PROCESS_ROOT_FOLDER + "/" + this.getPath() + "/out-data.csv";
 
 		if (Files.exists(Paths.get(inputParamFile))) {
 			Map<Object, Object> params = Files.lines(Paths.get(inputParamFile)).collect(Collectors.toMap(line -> line.split(",")[0], line -> line.split(",")[1])); // TODO Check type
 			try {
-				if (params.containsKey("rate")) rate = Float.parseFloat((String) params.get("rate"));				
+				if (params.containsKey("rate")) rate = Float.parseFloat((String) params.get("rate"));
+				else logger.info("Taux d'échantillonnage absent des paramètres, la valeur par défaut est utilisée");
 			} catch (Exception e) {
-				logger.error("Impossible d'interprêter le taux d'échantillonnage passé en paramètre : " + params.containsKey("rate"));
+				logger.error("Impossible d'interprêter le taux d'échantillonnage passé en paramètre : " + params.get("rate") + ", traitement abandonné");
+				return;
 			}
 		}
-
+		logger.info("Échantillonnage au taux " + rate + " du fichier " + inputDataFile);
 		try {
-			Stream<String> stream = Files.lines(Paths.get(inputFile)).filter(line -> random.nextFloat() < rate);
+			Stream<String> stream = Files.lines(Paths.get(inputDataFile)).filter(line -> random.nextFloat() < rate);
 			Files.write(Paths.get(outputDataFile), (Iterable<String>)stream::iterator);
+			logger.info("Échantillon sauvegardé dans le fichier " + outputDataFile);
 		} catch (Exception e) {
 			logger.error("Erreur dans le module d'échantillonnage : " + e.getMessage());
 		}
